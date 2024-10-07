ef36cb8f80ff2bb0004ee729c0b6d1d8450ca5b1
Student <andrii.kuliak@gmail.com>
2024-10-07 10:53:13 +0300

    Added practice #4

--- com/algoritmization/practice4/Main.java
+++ com/algoritmization/practice4/Main.java
@@ -1,5 +1,7 @@
 package com.college.algoritmization.com.algoritmization.practice4;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.printf("Hello world %n");
+    }
 }
